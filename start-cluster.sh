7b5481085bca058687aa56c43689517b30a2987b
Timur <timursayfullin@gmail.com>
2018-07-20 17:39:17 +0300

    automation

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+docker rm -f $(docker ps -qa)
+
 for (( i=1; i<=2; i++))
 do
     echo "Starting node$i"
-    docker run -d --name node$i --memory 2Gb node-image
+    docker run -d --name node$i -p 222$i:22 --memory 2Gb node-image
 done
--- stop-cluster.sh
+++ stop-cluster.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+echo $(docker ps -q)
+docker stop $(docker ps -q)
